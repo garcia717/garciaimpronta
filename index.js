bc27af8212d05b40ae9c01a2c1acd14d5534fc49
julio Garcia <garcia717@gmail.com>
2022-10-29 21:00:13 -0300

    boton ladilla

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector(".toggle")
-const navegacion = document.querySelector(".navegacion")
-
-toggle.addEventListener("click", () => {
-    navegacion.classList.toggle(".navegacion_visible");
-})
